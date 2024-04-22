38a10329d46903655725fe2684d887c92c73ad44
Mohamed Rabie <119837920+Mohamed-Rabie-Mohamed-Khedr@users.noreply.github.com>
2024-04-22 09:35:45 +0200

    5

--- CS_ToolsPuls/StringProcessing.cs
+++ CS_ToolsPuls/StringProcessing.cs
@@ -5,17 +5,91 @@
 public static class StringProcessing
 {
     /// <summary>
-    /// Counts the occurrences of a specified string within the current string.
+    /// Checks whether a string consists only of alphanumeric characters,
+    /// with an option to ignore specified characters.
+    /// </summary>
+    /// <param name="t">The string to check.</param>
+    /// <param name="ignoranceChars">Characters to ignore.</param>
+    /// <returns>True if the string is alphanumeric; otherwise, false.</returns>
+    public static bool IsAalphanumeric(this string t, params char[] ignoranceChars)
+    {
+        if (t.Length == 0) return false;
+        foreach (char c in t)
+        {
+            if (!char.IsDigit(c) && !char.IsLetter(c))
+            {
+                for (int i = 0; i < ignoranceChars.Length; i++)
+                {
+                    if (c == ignoranceChars[i]) break;
+                    else if (i == ignoranceChars.Length -1) return false;
+                }
+            }
+        }
+        return true;
+    }
+ 
+    /// <summary>
+    /// Checks whether a string consists only of alphanumeric characters.
+    /// </summary>
+    /// <param name="t">The string to check.</param>
+    /// <returns>True if the string is alphanumeric; otherwise, false.</returns>
+    public static bool IsAalphanumeric(this string t)
+    {
+        if (t == string.Empty) return false;
+        foreach (char c in t)
+        {
+            if (!char.IsDigit(c) && !char.IsLetter(c)) return false;
+        }
+        return true;
+    }
+
+    /// <summary>
+    /// Centers a string within a specified number of padding characters.
+    /// </summary>
+    /// <param name="t">The string to center.</param>
+    /// <param name="value">The padding character.</param>
+    /// <param name="count">The number of padding characters on each side.</param>
+    /// <returns>The centered string.</returns>
+    public static string Center(this string t, char value, int count)
+    {
+        if (count < 1) return t;
+        count *= 2;
+        StringBuilder Str = new StringBuilder(t.Length + count);
+        for (int i = 0; i < count; i++) Str.Append(value);
+        Str.Insert(Str.Length / 2, t);
+        return Str.ToString();
+    }
+
+    /// <summary>
+    /// Centers a string within a specified number of padding characters.
+    /// </summary>
+    /// <param name="t">The string to center.</param>
+    /// <param name="value">The padding string.</param>
+    /// <param name="count">The number of times the padding string should be added on each side.</param>
+    /// <returns>The centered string.</returns>
+    public static string Center(this string t, string value, int count)
+    {
+        if (count < 1) return t;
+        count *= 2;
+        StringBuilder Str = new StringBuilder(t.Length + value.Length * count);
+        for (int i = 0; i < count; i++) Str.Append(value);
+        Str.Insert(Str.Length / 2, t);
+        return Str.ToString();
+    }
+
+    /// <summary>
+    /// Counts the occurrences of a specified string within the current string, using a specified string comparison option.
     /// </summary>
     /// <param name="t">The current string.</param>
     /// <param name="value">The string to count.</param>
+    /// <param name="comparison">The string comparison option to use (default is StringComparison.Ordinal).</param>
     /// <returns>The number of occurrences of the specified string.</returns>
-    public static int ValueCount(this string t, string value)
+    public static int ValueCount(this string t, string value, StringComparison comparison = StringComparison.Ordinal)
     {
         int count = 0, start = 0, s = 0;
         while (true)
         {
-            s = t.IndexOf(value, start);
+            s = t.IndexOf(value, start, comparison);
             if (s != -1)
             {
                 count++;
--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -3,23 +3,9 @@
     class Program
     {
         static void Main(string[] args)
-        { 
-            string str = "hi hi, digit is 1 2, 3 and .5";
-            
-            string digitAsString;
-            uint digitAsUint;
-            double digitAsDouble;
-            
-            str.GetDigit(out digitAsString);
-            str.GetDigit(out digitAsUint);
-            str.GetDigit(out digitAsDouble);
-
-            Console.WriteLine(digitAsString);// => 1235
-            Console.WriteLine(digitAsUint);// => 11
-            Console.WriteLine(digitAsDouble);// => 6.5
-            Console.WriteLine(str.ValueCount('i'));// => 5
-            Console.WriteLine(str.ValueCount("hi"));// => 2
-            Console.WriteLine(str.ToTitleCase());// => Hi Hi, Digit Is 1 2, 3 And .5
+        {
+            string str = "";
+            Console.WriteLine(str.IsAalphanumeric());
         }
     }
 }
